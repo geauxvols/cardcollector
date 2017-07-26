771396ff767c278a7f12cee80b6ce9f3c8b0918c
David Maples <geauxvols@gmail.com>
2017-07-25 22:58:39 -0500

    Added UI element methods

---
+++ CardCollector/CardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CardViewController.swift
+//  CardCollector
+//
+//  Created by David Maples on 7/25/17.
+//  Copyright © 2017 daveapps. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    @IBOutlet weak var cardTitleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
